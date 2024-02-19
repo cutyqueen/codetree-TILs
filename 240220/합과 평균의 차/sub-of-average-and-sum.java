9ea7d85424fd336fb7dfeae2b5ed0238a682556a
cutyqueen <141491896+cutyqueen@users.noreply.github.com>
2024-02-20 00:02:51 +0900

    Title: 합과 평균의 차, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240220/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 10;
+        int b = 3;
+        int c = 5;
+
+        int i=a+b+c;
+        int m=(a+b+c)/3;
+        int n=i-m;
+        
+        System.out.println(i+"\n"+m+"\n"+n);
+    }
+}
