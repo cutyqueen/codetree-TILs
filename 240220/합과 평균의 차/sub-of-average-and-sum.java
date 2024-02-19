603ff891a79ec4712a0fa47d55c97d391845d818
cutyqueen <141491896+cutyqueen@users.noreply.github.com>
2024-02-20 00:05:01 +0900

    Title: 합과 평균의 차, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240220/합과 평균의 차/sub-of-average-and-sum.java
+++ 240220/합과 평균의 차/sub-of-average-and-sum.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a = 10;
-        int b = 3;
-        int c = 5;
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
         int i=a+b+c;
         int m=(a+b+c)/3;
         int n=i-m;
-        
+
         System.out.println(i+"\n"+m+"\n"+n);
     }
 }
