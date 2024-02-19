684a625d1968a98802ca06e66ad0efb6e135f4a7
cutyqueen <141491896+cutyqueen@users.noreply.github.com>
2024-02-19 22:33:15 +0900

    Title: 행 단위로 반복하여 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240219/행 단위로 반복하여 출력/print-in-row.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        for(int i=0;i<=n;i++) {
+            for(int j=0;j<=n;j++) {
+                System.out.print(j+1);
+            }
+        }
+        System.out.println();
+    }
+}
