8e6549883582ae0097c9993dcd946245200adfed
cutyqueen <141491896+cutyqueen@users.noreply.github.com>
2024-02-22 23:48:41 +0900

    Title: 등장횟수 확인, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        int count=0;
+
+        for(int i=0;i<5;i++) {
+            num = sc.nextInt();
+            if(num%2==0) {
+                count++;
+            }
+        }
+        System.out.print(count);
+
+    }
+}
