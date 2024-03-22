bee8ef4e46a7c54c58d68928a79081211ba98162
cutyqueen <141491896+cutyqueen@users.noreply.github.com>
2024-03-22 23:04:04 +0900

    Title: 가장 짧은 문자열, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240322/가장 짧은 문자열/shortest-string.java
+++ 240322/가장 짧은 문자열/shortest-string.java
@@ -1,31 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = sc.next();
-        String str3 = sc.next();
-
-        int a = str1.length();
-        int b = str2.length();
-        int c = str3.length();
-
-        if(a>b&& b>c) {
-            System.out.print(a-c);
-        } else if(a>c&& c>b) {
-            System.out.print(a-b);
-
-        } else if(b>c&& c>a) {
-            System.out.print(b-a);
-        } else if(b>a&& a>c) {
-            System.out.print(b-c);
-
-        } else if(c>a&& a>b) {
-            System.out.print(c-b);
-        } else{
-            System.out.print(c-a);
-        }
-        
+  public static void main(String[] args) {
+    
+    Scanner sc =new Scanner(System.in);
+    
+    String s1 = sc.next();
+    String s2 = sc.next();
+    String s3 = sc.next();
+    
+    int len[] = {s1.length(),s2.length(),s3.length()};
+    int min = len[0];
+    int max = len[0]; 
+    
+    for (int i = 0; i < len.length; i++) {
+      if(len[i]<=min) {
+        min=len[i];
+      }
+      if(len[i]>=max) {
+        max=len[i];
+      }
     }
+    System.out.println(max-min);
+    
+    
+  }
 }
